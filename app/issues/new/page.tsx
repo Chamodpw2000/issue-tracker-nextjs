87a6ca4f64805bffd9bb84c12c26c7a6677cac66
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-05-14 10:21:36 +0530

    added a mde editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,24 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+
+import { Button, TextField } from '@radix-ui/themes'
+import React, { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
+import "easymde/dist/easymde.min.css";
+
+// Dynamically import SimpleMDE with SSR disabled
+const SimpleMDE = dynamic(
+  () => import('react-simplemde-editor'),
+  { ssr: false }
+)
 
 const NewIssuePage = () => {
+
+
   return (
     <div className='max-w-xl space-y-3'>
-
-        <TextField.Root  placeholder="Enter the title of the issue" />
-           
-        <TextArea placeholder="Reply to comment…" />
-<Button>Submit</Button>
+      <TextField.Root placeholder="Enter the title of the issue" />
+      <SimpleMDE placeholder="Enter the description of the issue" />
+      <Button>Submit</Button>
     </div>
   )
 }
