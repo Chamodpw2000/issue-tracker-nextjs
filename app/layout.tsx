e8c69b041b42889d8f0fd54ad46f25ecc7bf20ce
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-05-14 10:06:08 +0530

    customize redix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 import "@radix-ui/themes/styles.css";
-import {Theme} from "@radix-ui/themes";
+import {Theme, ThemePanel} from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,14 +30,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme> 
+   <Theme accentColor="violet" grayColor="sand">
               <NavBar/>
+
           
            <main className="p-5">
 
         {children}
 
-        </main></Theme>
+        </main>
+        
+        
+        </Theme>
     
 
       
